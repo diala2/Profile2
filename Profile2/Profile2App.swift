eb741a222d4072ed9fb49428345df4f80b298cfb
Diala <dialafayoumi@gmail.com>
2024-09-29 10:04:29 +0300

    Initial Commit

---
+++ Profile2/Profile2App.swift
@@ -0,0 +1,17 @@
+//
+//  Profile2App.swift
+//  Profile2
+//
+//  Created by Diala Abdulnasser Fayoumi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Profile2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
